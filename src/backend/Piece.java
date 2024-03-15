a6bf745d984687fc8749f28c5fb04d682e1ffe49
SayaKrs <120859053+SayaKrs@users.noreply.github.com>
2024-03-15 04:57:52 -0400

    add a constructor and a helper method for Piece.java

--- src/backend/Piece.java
+++ src/backend/Piece.java
@@ -22,10 +22,39 @@ public abstract class Piece {
         this.pieceType = pieceType;
         this.isBlack = isBlack;
     }
+    protected Piece(PieceType pieceType, boolean isBlack, Location loc) {
+        this.pieceType = pieceType;
+        this.isBlack = isBlack;
+        this.loc = loc;
+    }
     //methods
     public abstract boolean isLegalMove(Location loc);
     public abstract Set<Location> listPossibleMoves();
     //boolean borderCheck(Location loc){}
     public boolean isBlack() {return isBlack;}
     public PieceType getType() {return pieceType;}
+
+    //helper methods
+    //col_shift makes horizontal move, row_shift makes vertical move
+    protected Set<Location> listLinearMoves(int col_shift, int row_shift) { //arg range {-1, 0, 1}
+        Set<Location> set = new HashSet<>();
+        char col_candidate = this.col;
+        int row_candidate = this.row;
+        col_candidate += col_shift;
+        row_candidate += row_shift;
+        while(col_candidate >= 'a' && col_candidate <= 'h' && row_candidate >= 1 && row_candidate <= 8)
+        {
+            if(board.isEmptyAt(col, row)) { //if candidate is empty
+                set.add(Location(col, row));
+            } else {    //if candidate is a piece
+                if(board.at(col, row).isBlack != this.isBlack) { //if candidate is opponent
+                    set.add(Location(col, row));
+                } else {} //do nothing
+                break;  //end search
+            }
+            col_candidate += col_shift;
+            row_candidate += row_shift;
+        }
+        return set;
+    }
 }
