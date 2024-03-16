585d9cacc07e569f67ae6c939534b5600b4feae7
SayaKrs <120859053+SayaKrs@users.noreply.github.com>
2024-03-16 02:36:47 -0400

    refactor methods by using Location.equals()

--- src/backend/ChessBoard.java
+++ src/backend/ChessBoard.java
@@ -15,19 +15,29 @@ public class ChessBoard {
 
     //methods
     public Piece at(Location loc) {
-        return at(loc.getCol(), loc.getRow());
-    }
-    public Piece at(char col, int row) {
-        if (col == maskSource.getCol() && row == maskSource.getRow()) {   //if query match mask source
+        if (loc == maskSource) {    //if query location match maskSource
             return null;
         }
-        if (col == maskGoal.getCol() && row == maskGoal.getRow()) {   //if query match mask goal
+        if (loc == maskGoal) {      //if query location match maskGoal
             return Piece[mask_source.getCol() - 'a'][mask_source.getRow() - a]; //return source Piece
         }
-        return Piece[col - 'a'][row - 1];
+        return Piece[loc.getCol() - 'a'][loc.getRow() - 1]; //return unmasked Piece
+    }
+    public Piece at(char col, int row) {
+        if (col == maskSource.getCol() && row == maskSource.getRow()) { //if query location match mask source
+            return null;
+        }
+        if (col == maskGoal.getCol() && row == maskGoal.getRow()) {     //if query location match mask goal
+            return Piece[mask_source.getCol() - 'a'][mask_source.getRow() - a]; //return source Piece
+        }
+        return Piece[col - 'a'][row - 1];   //return unmasked Piece
     }
     public boolean isEmptyAt(Location loc) {
-        return isEmptyAt(loc.getCol(), loc.getRow());
+        if (at(loc) == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
     public boolean isEmptyAt(char col, int row) {
         if (at(col, row) == null) {
